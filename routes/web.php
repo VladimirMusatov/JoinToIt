d94081c6783722cc0b73220662d5484600a1c5be
Vladimir Musatov <vovamusatov.2001@gmail.com>
2025-04-24 22:26:16 +0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -20,33 +20,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Companies
-Route::get('/companies', [CompaniesController::class, 'index'])->name('companies');
-// Function for table pagination
-Route::get('/companies-data', [CompaniesController::class, 'getData'])->name('companies-data');
-Route::get('/create-companies', [CompaniesController::class, 'create'])->name('create-companies');
-Route::post('/store-companies', [CompaniesController::class, 'store'])->name('store-companies');
-Route::get('/edit_company/{id}', [CompaniesController::class, 'edit'])->name('edit_company');
-Route::post('/update-company/{id}',[CompaniesController::class, 'update'])->name('update-company');
-Route::get('/delete_company/{id}', [CompaniesController::class, 'delete'])->name('delete_company');
+Route::middleware(['auth'])->group(function () {
+    // Companies
+    Route::get('/companies', [CompaniesController::class, 'index'])->name('companies');
+    // Function for table pagination
+    Route::get('/companies-data', [CompaniesController::class, 'getData'])->name('companies-data');
+    Route::get('/create-companies', [CompaniesController::class, 'create'])->name('create-companies');
+    Route::post('/store-companies', [CompaniesController::class, 'store'])->name('store-companies');
+    Route::get('/edit_company/{id}', [CompaniesController::class, 'edit'])->name('edit_company');
+    Route::post('/update-company/{id}',[CompaniesController::class, 'update'])->name('update-company');
+    Route::get('/delete_company/{id}', [CompaniesController::class, 'delete'])->name('delete_company');
 
-// Employees
-Route::get('/employees', [EmployeesController::class, 'index'])->name('employees');
-Route::get('/get-employees', [EmployeesController::class, 'getData'])->name('employees-data');
-Route::get('/create-employee', [EmployeesController::class, 'create'])->name('create-employee');
-Route::post('/store-employee', [EmployeesController::class, 'store'])->name('store-employee');
-Route::get('/edit-employee/{id}', [EmployeesController::class, 'edit'])->name('edit-employee');
-Route::post('/update-employee/{id}',[EmployeesController::class, 'update'])->name('update-employee');
-Route::get('/delete_employee/{id}', [EmployeesController::class, 'delete'])->name('delete_employee');
-
-Route::get('/dashboard', function () {
-    return redirect()->route('companies');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Employees
+    Route::get('/employees', [EmployeesController::class, 'index'])->name('employees');
+    // Function for table pagination
+    Route::get('/get-employees', [EmployeesController::class, 'getData'])->name('employees-data');
+    Route::get('/create-employee', [EmployeesController::class, 'create'])->name('create-employee');
+    Route::post('/store-employee', [EmployeesController::class, 'store'])->name('store-employee');
+    Route::get('/edit-employee/{id}', [EmployeesController::class, 'edit'])->name('edit-employee');
+    Route::post('/update-employee/{id}',[EmployeesController::class, 'update'])->name('update-employee');
+    Route::get('/delete_employee/{id}', [EmployeesController::class, 'delete'])->name('delete_employee');
 });
 
 require __DIR__.'/auth.php';
